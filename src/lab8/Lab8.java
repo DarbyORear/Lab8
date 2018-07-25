ca6f753a35a7c00863975c6e82c45998424775f6
DarbyORear <darbyorear@gmail.com>
2018-07-25 06:34:04 -0400

    added a do-while loop.

--- src/lab8/Lab8.java
+++ src/lab8/Lab8.java
@@ -11,16 +11,12 @@ public class Lab8 {
 		
 		
 //Create the dataset. Should consist of a list of students' names and two facts about each student (so, three parallel arrays total).
-		
-		//Make an array with at least 8 students.
-		
-//		if you want, you can use the Validator class we made.
 
 		String studentNum[] = {"1 = Kris", "\n 2 = Louis", "\n 3 = Sharde", "\n 4 = Virginia", "\n 5 = Amna", "\n 6 = Jasmine", "\n 7 = Shontinique", "\n 8 = Tim", "\n 9 = Arif", "\n 10 = Eddy"};
 		String names[] = {"Kris", "Louis", "Sharde", "Virginia", "Amna", "Jasmine", "Shontinique", "Tim", "Arif", "Eddy"};
 		String faveFoods[] = {"Thai", "Wasabi peas", "Japanese", "chocolate", "coffee", "Alfredo", "bread", "cookies", "pizza", "hamburgers"};
 		String faveAnimals[] = {"dog", "cat", "horse", "duck", "rabbit", "ferret", "guinea pig", "fox", "hamster", "flamingo"};
-	
+	    String userContinue;
 //Ask user what student they want to learn about.
 		System.out.println("Hello! Thanks for wanting to learn more about Grand Circus students. Here is a list of our students: ");
 		String studentNumString = Arrays.toString(studentNum);
@@ -28,6 +24,7 @@ public class Lab8 {
 		System.out.println();
 //	}
 //	
+		do {
 //		public static String learnSomethingNew(String randomName) {
 		System.out.println("Please enter the number for the student you want to learn more about: ");
 		Scanner scnr = new Scanner(System.in);
@@ -55,9 +52,10 @@ public class Lab8 {
 		
 		System.out.println("Would you like to learn another fact about a Grand Circus student? Please type yes or no: " );
 		System.out.println();
-		String userContinue = scnr.next();
+		userContinue = scnr.next();
+		}
 		
-		while (userContinue.equals("yes")) {    //use try-catch to include "y"?
+		while (userContinue.equals("yes")); //use try-catch to include "y"?
 			//LOOP
 			//method(lines 30-52).
 		}
@@ -66,4 +64,3 @@ public class Lab8 {
 	
 	}
 
-}
